cffa47cce48da27c95da989662c76624d5fcc87e
John Pazarzis <jpazarzis@rapidsos.com>
2025-06-26 01:00:36 -0400

    Improves header.

--- headers/score_matrix.h
+++ headers/score_matrix.h
@@ -30,7 +30,7 @@
 #define __SCORE_MATRIX_H__
 
 #include <string>
-#include <utility> // std::pair
+#include <utility>
 #include <vector>
 
 
@@ -56,58 +56,10 @@ public:
     ~ScoreMatrix() = default;
 
     // Disable copy and move operations
-    ScoreMatrix(const ScoreMatrix&) = delete;            // Copy constructor
-    ScoreMatrix& operator=(const ScoreMatrix&) = delete; // Copy assignment
-    ScoreMatrix(ScoreMatrix&&) = delete;                 // Move constructor
-    ScoreMatrix& operator=(ScoreMatrix&&) = delete;      // Move assignment
-
-    std::string getSequence1() const;
-    std::string getSequence2() const;
-    std::pair<int, int> getMaxPosition() const;
-    std::vector<std::string> getLocalAlignment() const;
-    int getRowCount() const;
-    int getColCount() const;
-    int getScore(int row, int col) const;
-    std::string to_str() const;
-};
-
-
-#endif // __SCORE_MATRIX_H__
-
-#ifndef __SCORE_MATRIX_H__
-#define __SCORE_MATRIX_H__
-
-#include <string>
-#include <utility> // std::pair
-#include <vector>
-
-
-class ScoreMatrix {
-
-private:
-    const std::string m_sequence1;
-    const std::string m_sequence2;
-    const int m_match_score;
-    const int m_mismatch_penalty;
-    const int m_gap_penalty;
-
-    mutable std::vector<std::pair<int, int>> m_max_positions;
-    mutable std::pair<int, int> m_max_position;
-    mutable std::vector<std::vector<int>> m_matrix;
-    mutable std::vector<std::string> m_local_alignments;
-
-    void initializeMatrix() const;
-    void traceback(int row, int col, std::string x1, std::string x2, std::string a) const;
-public:
-    ScoreMatrix(const std::string& s1, const std::string& s2,
-            int match_score, int mismatch_penalty, int gap_penalty);
-    ~ScoreMatrix() = default;
-
-    // Disable copy and move operations
-    ScoreMatrix(const ScoreMatrix&) = delete;            // Copy constructor
-    ScoreMatrix& operator=(const ScoreMatrix&) = delete; // Copy assignment
-    ScoreMatrix(ScoreMatrix&&) = delete;                 // Move constructor
-    ScoreMatrix& operator=(ScoreMatrix&&) = delete;      // Move assignment
+    ScoreMatrix(const ScoreMatrix&) = delete;
+    ScoreMatrix& operator=(const ScoreMatrix&) = delete;
+    ScoreMatrix(ScoreMatrix&&) = delete;
+    ScoreMatrix& operator=(ScoreMatrix&&) = delete;
 
     std::string getSequence1() const;
     std::string getSequence2() const;
