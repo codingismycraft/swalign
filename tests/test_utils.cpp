9f249d23884d0c373a5b1279e768cac44696b993
John Pazarzis <jpazarzis@rapidsos.com>
2025-06-29 00:50:15 -0400

    Completes the test_utils.cpp

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -13,22 +13,10 @@ std::filesystem::path get_source_dir() {
 int main(){
     std::cout << "Testing the utils component" << std::endl;
     const std::filesystem::path source_dir = get_source_dir();
-    std::cout << source_dir << std::endl;
 
      // Create the path to your file
     std::filesystem::path file_path = std::filesystem::current_path() / "tests" / "data" / "seq1.txt";
+    std::string chromosome = readFastaSequence(file_path.string());
+    std::cout << chromosome << std::endl;
 
-    // Open the file
-    std::ifstream infile(file_path);
-
-    if (!infile) {
-        std::cerr << "Failed to open file: " << file_path << std::endl;
-        return 1;
-    }
-
-    // Read and print the file line by line
-    std::string line;
-    while (std::getline(infile, line)) {
-        std::cout << line << '\n';
-    }
 }
