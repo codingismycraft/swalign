59834e5136dac40a9ba88e1dfb440e9db1d6a17c
John Pazarzis <jpazarzis@rapidsos.com>
2025-08-02 18:32:21 -0400

    Improves test_big_array.

--- tests/test_big_array.cpp
+++ tests/test_big_array.cpp
@@ -1,29 +1,46 @@
 //  Tests the BigArray class.
 //
+#include <assert.h>
 #include <iostream>
 #include "big_array.h"
 
+#define TESTING_FILE_NAME "test_file.bin"
+#define ROWS 50
+#define COLS 50
+
+
+void create_file() {
+    auto p_big_array = BigArray::make_new(TESTING_FILE_NAME, ROWS, COLS);
+    for (size_t row = 0; row < ROWS; ++row) {
+        for (size_t col = 0; col < COLS; ++col) {
+            p_big_array->set( row, col, row * COLS + col);
+        }
+    }
+    for (size_t row = 0; row < ROWS; ++row) {
+        for (size_t col = 0; col < COLS; ++col) {
+            const auto expected = int32_t(row * COLS + col);
+            const auto retrieved = p_big_array->get( row, col);
+            assert(retrieved == expected);
+        }
+    }
+}
+
+
+void read_file() {
+    auto p_big_array = BigArray::load(TESTING_FILE_NAME);
+
+    for (size_t row = 0; row < ROWS; ++row) {
+        for (size_t col = 0; col < COLS; ++col) {
+            const auto expected = int32_t(row * COLS + col);
+            const auto retrieved = p_big_array->get( row, col);
+            assert(retrieved == expected);
+        }
+    }
+}
+
 
 int main(){
-
-    const size_t ROWS = 5;
-    const size_t COLS = 5;
-
-
-    auto p_big_array = BigArray::make_new("test_file.bin", ROWS, COLS);
-    std::cout << p_big_array->get_filename() << std::endl;
-
-    for (size_t row = 0; row < ROWS; ++row) {
-        for (size_t col = 0; col < COLS; ++col) {
-            p_big_array->set( row, col, row*col + col);
-        }
-    }
-
-    for (size_t row = 0; row < ROWS; ++row) {
-        for (size_t col = 0; col < COLS; ++col) {
-            const int v = p_big_array->get( row, col);
-            std::cout << "Value at (" <<row  << ", " << col << ") = " << v << std::endl;
-
-        }
-    }
+    create_file();
+    read_file();
+    std::cout << "BigArray: all tests passed!" << std::endl;
 }
